f771ad663b1ad3c0b9370f466a606f18bd752b5d
Diky Putra <joykey.grapichdiky@gmail.com>
2021-10-07 20:07:10 +0700

    minggu4

---
+++ MySpinnerE41202167/app/src/main/java/com/dpe/myspinnere41202167/MainActivity.java
@@ -0,0 +1,24 @@
+package com.dpe.myspinnere41202167;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
